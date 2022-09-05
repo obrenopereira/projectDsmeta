c50db5818163fcbd6bc6620d123bce424ac9e69a
Breno Pereira <brenopereiragms@gmail.com>
2022-09-05 18:41:31 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/projectDsmeta/ProjectDsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projectDsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDsmetaApplication.class, args);
+	}
+
+}
